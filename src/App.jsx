cf292463dbe8e2bbf7675e7f6795d09a0f4ed173
Utopianboy99 <madnhlaka@gmail.com>
2025-04-02 08:58:48 +0200

    change challenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Invitation from './components/invitation'
 import './App.css'
+// import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <>
      <Invitation/>
+     {/* <Analytics/> */}
     </>
   )
 }
