d7c2a39a50cf2cd41a864be6b22962e559dd5e39
Utopianboy99 <madnhlaka@gmail.com>
2025-04-02 09:10:44 +0200

    same

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Invitation from './components/invitation'
 import './App.css'
-// import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <>
      <Invitation/>
-     {/* <Analytics/> */}
+     <Analytics/>
     </>
   )
 }
